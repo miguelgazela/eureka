632693debed34b5746a85fe0d2c98fbbe3d2dac7
miguelgazela <miguel.gazela@gmail.com>
2013-12-08 20:12:17 +0000

    Sign in form completed with error warnings

--- eureka/ideas/views.py
+++ eureka/ideas/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
 from django.http import HttpResponse
-from django.contrib.auth import authenticate
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
 from ideas.forms import UserCreationForm
+from ideas.forms import LoginForm
 
 # Create your views here.
 
@@ -15,18 +15,13 @@ def login(request):
     if request.method == 'GET':
         return render(request, 'ideas/auth/login.html')
     elif request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            if user.is_active:
+        login_form = LoginForm(request.POST)
+        if request.POST and login_form.is_valid():
+            user = login_form.login(request)
+            if user:
                 auth_login(request, user)
                 return redirect('index')
-            else:
-                return HttpResponse("You're account is disabled")
-        else:
-            return HttpResponse("Invalid login!")
+        return render(request, 'ideas/auth/login.html', {'login_form': login_form})
 
 def logout(request):
     auth_logout(request)
@@ -47,5 +42,4 @@ def signup(request):
             auth_login(request, user)
             return redirect('index')
         else:
-            context = {'form': user_form}
-            return render(request, 'ideas/auth/signup.html', context)
+            return render(request, 'ideas/auth/signup.html', {'form': user_form})
